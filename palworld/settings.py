b749afff4c9461326b9fb7dcd20ef51f941447ec
Mikechavo <mchavez76@yahoo.com>
2024-02-17 17:37:43 -0600

    updating for vercel deployment

--- palworld/settings.py
+++ palworld/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&ol+(noozhpak2!3s)b8kl8rqnc-4t71mhij2k@!%j7nhi_$s8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
