000574479eea30b8ee9b83ddfd0d504f44dc87ea
Mikechavo <mchavez76@yahoo.com>
2024-02-22 05:51:28 -0600

    quick fix

--- members/models.py
+++ members/models.py
@@ -1,28 +1,28 @@
-# from django.db import models
-# from django.contrib.auth.models import User
-
-
-# class FireTeam(models.Model):
-#     name = models.CharField(max_length=100)
-#     leader = models.ForeignKey(User, on_delete=models.CASCADE, related_name='fire_teams_led')
-#     members = models.ManyToManyField(, related_name='fire_teams_joined', default=list)
-
-#     def __str__(self):
-#         return self.name
-
 from django.db import models
 from django.contrib.auth.models import User
-# from django.core.serializers.json import DjangoJSONEncoder
-# import json
+from paldex.models import PalModel 
 
 class FireTeam(models.Model):
     name = models.CharField(max_length=100)
     leader = models.ForeignKey(User, on_delete=models.CASCADE, related_name='fire_teams_led')
-    members = models.JSONField(default=list)  # Change to JSONField
+    members = models.ManyToManyField(PalModel , related_name='fire_teams_joined', default=list)
 
     def __str__(self):
         return self.name
 
-    def num_members(self):
-        return len(self.members)
+# from django.db import models
+# from django.contrib.auth.models import User
+# # from django.core.serializers.json import DjangoJSONEncoder
+# # import json
+
+# class FireTeam(models.Model):
+#     name = models.CharField(max_length=100)
+#     leader = models.ForeignKey(User, on_delete=models.CASCADE, related_name='fire_teams_led')
+#     members = models.JSONField(default=list)  # Change to JSONField
+
+#     def __str__(self):
+#         return self.name
+
+#     def num_members(self):
+#         return len(self.members)
 
