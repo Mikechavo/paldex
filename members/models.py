6a23a8c0dc741313cd7efeb0625bd8ec21ae3e3f
Mikechavo <mchavez76@yahoo.com>
2024-02-21 18:52:49 -0600

    halfway through members

--- members/models.py
+++ members/models.py
@@ -12,17 +12,12 @@
 
 from django.db import models
 from django.contrib.auth.models import User
-# from django.core.serializers.json import DjangoJSONEncoder
-# import json
+from paldex.models import PalModel  # Assuming PalModel is your Pal model
 
 class FireTeam(models.Model):
     name = models.CharField(max_length=100)
     leader = models.ForeignKey(User, on_delete=models.CASCADE, related_name='fire_teams_led')
-    members = models.JSONField(default=list)  # Change to JSONField
+    members = models.ManyToManyField(PalModel, related_name='fire_teams_joined')
 
     def __str__(self):
         return self.name
-
-    def num_members(self):
-        return len(self.members)
-
